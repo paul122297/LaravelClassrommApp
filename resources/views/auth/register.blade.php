2e16680cc5133145e99fd0775b71874390ffeb2c
Paul Marquita <pamarquita@gmail.com>
2019-03-15 18:57:15 +0800

    info updated

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,10 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header">Invalid</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
+                    {{-- <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="form-group row">
@@ -68,7 +68,7 @@
                                 </button>
                             </div>
                         </div>
-                    </form>
+                    </form> --}}
                 </div>
             </div>
         </div>
